4b5ba4f5f1fd7285b248f883372ce105ed446a9f
vlad <vlad.munteanu@orange.com>
2022-06-09 19:00:50 +0300

    feat: access granted via password

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ import {
 } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { prices } from "./prices";
+import { password } from "./secrets";
 
 export enum Car {
   Camion = "Camion",
@@ -35,6 +36,7 @@ prices.forEach(row => {
 });
 
 export const App = () => {
+  const [accessGranted, setAccessGranted] = React.useState<boolean>(false);
   const [importingCountry, setImportingCountry] = React.useState<string>("");
   const [exportingCountry, setExportingCountry] = React.useState<string>("");
   const [grupaj, setGrupaj] = React.useState(false);
@@ -187,115 +189,129 @@ export const App = () => {
   return (
     <ChakraProvider theme={theme}>
       <Center className="App" w="100vw" h="100vh" overflow="hidden">
-        <VStack
-          w={{ base: "100%", md: "50%" }}
-          gap="10px"
-          boxShadow={"dark-lg"}
-          px="20"
-          py="10"
-          borderRadius={"3xl"}
-        >
-          <Text fontFamily={""} textAlign={"center"} fontSize="5xl">
-            Transportation Calculator
-          </Text>
-          <ColorModeSwitcher />
-          <HStack justify={"space-around"} width="100%">
-            <FormControl>
-              <FormLabel htmlFor="km">De la</FormLabel>
-              <Select
-                placeholder="Select a country"
-                value={importingCountry}
-                onChange={changeImportingCountry}
-              >
-                {countries.map(country => (
-                  <option value={country}>{country}</option>
-                ))}
-              </Select>
-            </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="km">Catre</FormLabel>
-              <Select
-                placeholder="Select a country"
-                value={exportingCountry}
-                onChange={changeExportingCountry}
-              >
-                {countries.map(country => (
-                  <option key={country} value={country}>
-                    {country}
-                  </option>
-                ))}
-              </Select>
-            </FormControl>
-          </HStack>
-          <HStack justify={"space-around"}>
-            <Select onChange={toggleCarType} placeholder="Choose Car">
-              <option value="Sprinter">Sprinter</option>
-              <option value="Camion">Camion</option>
-            </Select>
-            <Checkbox
-              size="lg"
-              onChange={() => {
-                setGrupaj(!grupaj);
+        {/* commenteaza daca scoti logarea cu parola */}
+        {accessGranted === false ? (
+          <FormControl width="50%">
+            <FormLabel>Parola</FormLabel>
+            <Input
+              type="password"
+              onChange={e => {
+                if (e.target.value === password) setAccessGranted(true);
               }}
-            >
-              Grupaj
-            </Checkbox>
-          </HStack>
-          {carType !== Car.Camion && carType !== Car.Sprinter ? (
-            <Text color="red.500" fontSize="lg">
-              Alege tipul de vehicul
+            ></Input>
+          </FormControl>
+        ) : (
+          //
+          <VStack
+            w={{ base: "100%", md: "50%" }}
+            gap="10px"
+            boxShadow={"dark-lg"}
+            px="20"
+            py="10"
+            borderRadius={"3xl"}
+          >
+            <Text fontFamily={""} textAlign={"center"} fontSize="5xl">
+              Transportation Calculator
             </Text>
-          ) : grupaj === false ? (
-            <FormControl>
-              <FormLabel htmlFor="km">Kilometrii</FormLabel>
-              <Input
-                id="km"
-                type="number"
-                value={kilometers}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  setKilometers(Number(e.target.value))
-                }
-              />
-            </FormControl>
-          ) : (
-            <>
-              {carType === Car.Camion && (
-                <FormControl>
-                  <FormLabel htmlFor="m">Metrii</FormLabel>
-                  <Input
-                    id="m"
-                    type="number"
-                    value={m}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                      setM(Number(e.target.value))
-                    }
-                  />
-                </FormControl>
-              )}
-              {carType === Car.Sprinter && (
-                <FormControl>
-                  <FormLabel htmlFor="kg">Kg</FormLabel>
-                  <Input
-                    id="kg"
-                    type="number"
-                    value={kg}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                      setKg(Number(e.target.value))
-                    }
-                  />
-                </FormControl>
-              )}
-            </>
-          )}
-          <Button onClick={calculatePrice}>Calculate</Button>
-          {error !== "" ? (
-            <Text fontSize={"lg"} color="red.500">
-              {error}
-            </Text>
-          ) : (
-            <Text fontSize={"2xl"}>{result}</Text>
-          )}
-        </VStack>
+            <ColorModeSwitcher />
+            <HStack justify={"space-around"} width="100%">
+              <FormControl>
+                <FormLabel htmlFor="km">De la</FormLabel>
+                <Select
+                  placeholder="Select a country"
+                  value={importingCountry}
+                  onChange={changeImportingCountry}
+                >
+                  {countries.map(country => (
+                    <option value={country}>{country}</option>
+                  ))}
+                </Select>
+              </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="km">Catre</FormLabel>
+                <Select
+                  placeholder="Select a country"
+                  value={exportingCountry}
+                  onChange={changeExportingCountry}
+                >
+                  {countries.map(country => (
+                    <option key={country} value={country}>
+                      {country}
+                    </option>
+                  ))}
+                </Select>
+              </FormControl>
+            </HStack>
+            <HStack justify={"space-around"}>
+              <Select onChange={toggleCarType} placeholder="Choose Car">
+                <option value="Sprinter">Sprinter</option>
+                <option value="Camion">Camion</option>
+              </Select>
+              <Checkbox
+                size="lg"
+                onChange={() => {
+                  setGrupaj(!grupaj);
+                }}
+              >
+                Grupaj
+              </Checkbox>
+            </HStack>
+            {carType !== Car.Camion && carType !== Car.Sprinter ? (
+              <Text color="red.500" fontSize="lg">
+                Alege tipul de vehicul
+              </Text>
+            ) : grupaj === false ? (
+              <FormControl>
+                <FormLabel htmlFor="km">Kilometrii</FormLabel>
+                <Input
+                  id="km"
+                  type="number"
+                  value={kilometers}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setKilometers(Number(e.target.value))
+                  }
+                />
+              </FormControl>
+            ) : (
+              <>
+                {carType === Car.Camion && (
+                  <FormControl>
+                    <FormLabel htmlFor="m">Metrii</FormLabel>
+                    <Input
+                      id="m"
+                      type="number"
+                      value={m}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        setM(Number(e.target.value))
+                      }
+                    />
+                  </FormControl>
+                )}
+                {carType === Car.Sprinter && (
+                  <FormControl>
+                    <FormLabel htmlFor="kg">Kg</FormLabel>
+                    <Input
+                      id="kg"
+                      type="number"
+                      value={kg}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        setKg(Number(e.target.value))
+                      }
+                    />
+                  </FormControl>
+                )}
+              </>
+            )}
+            <Button onClick={calculatePrice}>Calculate</Button>
+            {error !== "" ? (
+              <Text fontSize={"lg"} color="red.500">
+                {error}
+              </Text>
+            ) : (
+              <Text fontSize={"2xl"}>{result}</Text>
+            )}
+          </VStack>
+        )}
       </Center>
     </ChakraProvider>
   );
